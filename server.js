02b96ea33fcd222c8c130f9096499cf33e9e8eda
JOSHUA TRIMBLE <JAYTRIM6@GMAIL.COM>
2021-01-26 18:17:56 -0600

    using bodyParser

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ require('dotenv').config();
 // Define middleware here
 app.use(morgan('dev'))
 // app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+// app.use(bodyParser.json());;
 app.use(methodOverride('_method'))
-app.use(express.json());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // console.log('Limit file size: '+limit);
 
 
@@ -286,7 +286,7 @@ function isInvalidRange (start, end, maxIdx) {
 app.post('/upload/:videoTitle/:videoDesc/:equipmentNeeded/:fitnessLevel/:workoutCategory/:sampleVideo', upload.single('file'), (req, res) => {
   // res.json({file: req.file})
   console.log('File sending: ', req.file)
-  debugger;
+  // debugger;
   gfs.files.update({'filename': req.file.filename}, 
     {'$set': 
       {
